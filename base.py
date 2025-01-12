931cda799f996610113481e8b3721d8f48051278
ThePrimeagen <samueljamieson02@gmail.com>
2025-01-12 20:49:41 +0000

    set up the db with the id and quote columns

--- base.py
+++ base.py
@@ -7,3 +7,25 @@ app = Flask(__name__)
 
 app.config['SECRECT_KEY'] = 'idkwhattoputhere'
 
+
+###
+#dbstuff
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+
+class quotes(db.Model):
+    id = db.Column(db.Integer, primay_key = True)
+    quote = db.Column(db.Text)
+
+    def __init__(self,quote):
+        self.quote = quote
+
+    def __repr__(self):
+        return self.quote
